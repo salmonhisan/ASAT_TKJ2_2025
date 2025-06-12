6ab676bd5b03454265962e5d2569896a31606ce4
Salmakhoirotunhisan <salmahisan76@gmail.com>
2025-06-12 17:30:05 +0700

    Add files via upload

---
+++ remidi 2_Salma khoirotunHisan_33.py
@@ -0,0 +1,14 @@
+def luas_persegi(s): return s * s
+def luas_lingkaran(r): return 3.14 * r * r
+
+print("1. Luas Persegi\n2. Luas Lingkaran")
+p = int(input("Pilihan Anda: "))
+
+if p == 1:
+    s = float(input("Masukkan sisi: "))
+    print(f"Luas persegi dengan sisi {s} adalah {luas_persegi(s)}")
+elif p == 2:
+    r = float(input("Masukkan jari-jari: "))
+    print(f"Luas lingkaran dengan jari-jari {r} adalah {luas_lingkaran(r):.2f}")
+else:
+    print("Pilihan tidak valid.")
